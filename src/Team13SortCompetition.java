25cf8a4a87861d85c88de8bdeb0bc880fd5e3928
BT_1N3_23 <sfaruk3131@bths.edu>
2018-11-28 10:03:28 -0500

    2

--- src/Team13SortCompetition.java
+++ src/Team13SortCompetition.java
@@ -1,2 +1,24 @@
 public class Team13SortCompetition {
+    public int challengeOne(int[] arr)
+    {
+        int x = 0;
+        int pos1 = arr[x];
+        int pos2 = arr[x + 1];
+        int temp = 0;
+
+        for(int i = 0 ; i < arr.length ; i++)
+        {
+            if(pos1 < pos2)
+            {
+                x++;
+            }
+            else
+            {
+                temp = pos1;
+                pos1 = pos2;
+                pos2 = temp;
+                x++;
+            }
+        }
+    }
 }
